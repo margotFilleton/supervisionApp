757805c127a6b4f3fa831edd784369a55d602735
margotfilleton <margot.filleton@gmail.com>
2017-12-12 17:06:20 +0100

    Add Computer class

---
+++ src/supervisionApp/Computer.java
@@ -0,0 +1,43 @@
+package supervisionApp;
+import java.util.List;
+
+public class Computer {
+	
+	private List<Process> processList;
+	private float percentageCPU;
+	private float memoryUsed;
+	private float memoryMax;
+	
+	/**
+	 * @return the processList
+	 */
+	public List<Process> getProcessList() {
+		//TODO
+		return processList;
+	}
+	
+	/**
+	 * @return the percentageCPU
+	 */
+	public float getPercentageCPU() {
+		//TODO
+		return percentageCPU;
+	}
+	
+	/**
+	 * @return the memoryUsed
+	 */
+	public float getMemoryUsed() {
+		//TODO
+		return memoryUsed;
+	}
+	
+	/**
+	 * @return the memoryMax
+	 */
+	public float getMemoryMax() {
+		//TODO
+		return memoryMax;
+	}
+
+}
