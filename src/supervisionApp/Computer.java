76bea045830d723e0f5f03b6d6ff8b3e5a2108b2
Unknown <darkozedu78@gmail.com>
2018-01-09 09:03:08 +0100

    Change

--- src/supervisionApp/Computer.java
+++ src/supervisionApp/Computer.java
@@ -3,7 +3,6 @@ package supervisionApp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import supervisionApp.ihm.controller.SupervisionController;
 import supervisionApp.ihm.model.CPUInformation;
--- src/supervisionApp/Process.java
+++ src/supervisionApp/Process.java
@@ -1,59 +1,72 @@
 package supervisionApp;
 
 public class Process {
-	
+
 	private String name;
 	private float CPU;
 	private float memory;
 	private float disk;
 	private float network;
 	private boolean isStart;
-	private float size; 
-	
+	private float size;
+
 	/**
 	 * @return the name
 	 */
 	public String getName() {
 		return name;
 	}
+
 	/**
 	 * @return the cPU
 	 */
 	public float getCPU() {
 		return CPU;
 	}
+
 	/**
 	 * @return the memory
 	 */
 	public float getMemory() {
 		return memory;
 	}
+
 	/**
 	 * @return the disk
 	 */
 	public float getDisk() {
 		return disk;
 	}
+
 	/**
 	 * @return the network
 	 */
 	public float getNetwork() {
 		return network;
 	}
-	
+
 	/**
 	 * @return the isStart
 	 */
 	public boolean isStart() {
 		return isStart;
 	}
+
 	/**
-	 * @param isStart the isStart to set
+	 * @return the size
+	 */
+	public float getSize() {
+		return size;
+	}
+
+	/**
+	 * @param isStart
+	 *            the isStart to set
 	 */
 	public void setStart(boolean isStart) {
 		this.isStart = isStart;
 	}
-	
+
 	/**
 	 * @param name
 	 * @param CPU
@@ -70,23 +83,22 @@ public class Process {
 		this.network = network;
 		this.isStart = isStart;
 	}
-	
+
 	public Process(String name, float size, boolean isStart) {
 		this.name = name;
 		this.size = size;
 		this.isStart = isStart;
 	}
 
-	
 	public void killProcess() {
-		//TODO
-		
+		// TODO
+
 		isStart = false;
 	}
-	
+
 	public void restartProcess() {
-		//TODO
-		
+		// TODO
+
 		isStart = true;
 	}
 }
